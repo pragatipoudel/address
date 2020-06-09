f990f56a2d459fa0ed2fd15128794c422e1a454c
Pragati Poudel <pp0048@uah.edu>
2020-06-09 14:11:23 -0500

    Replace place with search

--- address.py
+++ address.py
@@ -3,7 +3,7 @@ import webbrowser
 
 # read selected text
 sel = os.popen('xsel').read()
-url = 'https://google.com/maps/place/' + sel
+url = 'https://google.com/maps/search/' + sel
 
 # open new tab from selected text url
 webbrowser.open_new_tab(url)
